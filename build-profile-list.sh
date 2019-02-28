41ecab5c6f4a28e96cf989f82408520ec2f46f49
Patrick Langford <patrick@neuronsong.com>
2019-02-28 12:54:48 -0700

    script updates

--- build-profile-list.sh
+++ build-profile-list.sh
@@ -2,4 +2,4 @@
 #
 for x in `ls output/StructureDefinition-*-definitions.html | sed -e 's/output\///' | cut -d'-' -f2`;do
   echo '<a href="StructureDefinition-'$x'-definitions.html">'$x'</a><br/>'
-done
+done | sort
--- build-valueset-list.sh
+++ build-valueset-list.sh
@@ -2,4 +2,4 @@
 #
 for x in `ls output/ValueSet-*.html | grep -v 'json' | grep -v 'xml' | grep -v 'ttl' | sed -e 's/output\///' | cut -d'-' -f2 | sed -e 's/.html//'`;do
   echo '<a href="ValueSet-'$x'.html">'$x'</a><br/>'
-done
+done | sort
